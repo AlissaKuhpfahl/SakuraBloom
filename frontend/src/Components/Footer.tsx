e77a8ba902a70ad587f36c75709395620d252174
Souher Al-Taleb <altalebsouher8@gmail.com>
2026-01-29 15:28:39 +0100

    ordner struktur

---
+++ frontend/src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      {" "}
+      <h1> Footer Bereich</h1>
+    </footer>
+  );
+}
---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router";
+
+import Footer from "../components/Footer.tsx";
+import Header from "../components/Header.tsx";
+
+export default function MainLayout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
