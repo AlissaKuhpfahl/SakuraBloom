8f6cc5737bba50f5cc1b9b0ec39d6e518da2bfa6
Alissa <alissa.samara@my.mail.de>
2026-01-30 16:48:56 +0100

    Login & Signup

--- frontend/src/Pages/Signup.tsx
+++ frontend/src/Pages/Signup.tsx
@@ -50,10 +50,10 @@ export function SignUp() {
               <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
             </svg>
             <input
-              name="Vorname"
+              className={inputClass}
+              name="firstName"
               value={firstName}
               onChange={handleChange}
-              className={inputClass}
               placeholder="Vorname"
             />
           </label>
@@ -67,7 +67,7 @@ export function SignUp() {
               <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
             </svg> */}
             <input
-              name="Nachname"
+              name="lastName"
               value={lastName}
               onChange={handleChange}
               className={inputClass}
