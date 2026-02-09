7ca6842efab679d6d6b71a97eb90c90608ca8a31
Alissa <alissa.samara@my.mail.de>
2026-02-09 10:14:18 +0100

    Work on profile selection

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,8 +1,14 @@
+export type Profile = {
+  profileName: string;
+  avatarUrl: string;
+};
+
 declare global {
   type User = {
     email: string;
     firstName: string;
     lastName: string;
     roles?: string[];
+    profiles?: Profile[];
   };
 }
