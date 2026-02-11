0698f99077ad04ecfd54e907fb2cd4571e1ce2a5
Alissa <alissa.samara@my.mail.de>
2026-02-11 17:07:25 +0100

    Typescript error cleanup

--- frontend/src/pages/Lessons.tsx
+++ frontend/src/pages/Lessons.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import PrimaryButton from "../components/Btn.tsx";
-import { isLessonDone } from "../utils/progress.tsx";
+import { isLessonDone } from "../utils/progress.ts";
 
 type ModuleKey = "online" | "privacy" | "chats" | "fake";
 type LessonStatus = "done" | "active" | "locked";
