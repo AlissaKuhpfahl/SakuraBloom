d900022c9e735012585f506109b9c5c6e77079b9
Alissa <alissa.samara@my.mail.de>
2026-02-12 14:02:39 +0100

    Implement function addProfile

--- frontend/src/data/profiles.ts
+++ frontend/src/data/profiles.ts
@@ -1,22 +1,45 @@
 import { backendServiceURL } from "../utils";
 
 export async function addProfile(profileName: string, avatarUrl: string, id: string) {
-  // const { email, password } = body;
-
   const res = await fetch(`${backendServiceURL}/profiles/progress/${id}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      // email,
-      // password
+      profileName,
+      avatarUrl
     }),
     credentials: "include"
   });
 
   if (!res.ok) {
     const errorData = await res.json();
+    console.log(errorData);
+    if (!errorData.error) {
+      throw new Error(errorData.message);
+    }
+    throw new Error(errorData.error);
+  }
+  return res.json();
+}
+
+export async function readProfiles(profileName: string, avatarUrl: string, id: string) {
+  const res = await fetch(`${backendServiceURL}/profiles/progress/${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      profileName,
+      avatarUrl
+    }),
+    credentials: "include"
+  });
+
+  if (!res.ok) {
+    const errorData = await res.json();
+    console.log(errorData);
     if (!errorData.error) {
       throw new Error(errorData.message);
     }
